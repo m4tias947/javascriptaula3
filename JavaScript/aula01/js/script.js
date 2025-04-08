e4aef8666ed56129ef5ef9f796d57bb036ee69a5
m4tias947 <diasmatias947@gmail.com>
2025-04-08 16:58:52 -0300

    versão01

---
+++ JavaScript/aula01/js/script.js
@@ -0,0 +1,15 @@
+document.getElementById("demo").innerHTML=("Matias")
+function aumentar(){
+    document.getElementById("demo").style.fontSize = "35px"
+}
+    function diminuir(){
+    document.getElementById("demo").style.fontSize = "15px"
+    }
+    function desligar(){
+      document.getElementById("lampada").src="img/vector-isolated-realistic-light-bulb-white/41931.jpg";
+
+    }
+    function ligar(){
+        document.getElementById("lampada").src="img/realistic-lit-light-bulb-isolated/41774.jpg"
+        
+    }
---
+++ JavaScript/desafio/js/script.js
@@ -0,0 +1,10 @@
+
+    function sol(){
+      document.getElementById("tempo").src="img/images.jpg";
+      document.getElementById("tempo1").src="img/transferir.jpg";
+    }
+    function chuva(){
+        document.getElementById("tempo").src="img/chuva).jpg";
+        document.getElementById("tempo1").src="img/chuva.jpg";
+    }
+    
