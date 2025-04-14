ba9546996b3a59da5662179677e41879544b24cd
m4tias947 <diasmatias947@gmail.com>
2025-04-14 17:52:48 -0300

    vs3

---
+++ aula2/js/script.js
@@ -0,0 +1,21 @@
+document.getElementById("btnMulher").
+addEventListener("click", function(){
+    document.getElementById("header").style.
+    backgroundColor = "blueviolet"
+     document.getElementById("footer").style.
+    backgroundColor = "blueviolet"
+});
+document.getElementById("btnHomem").
+addEventListener("click", function(){
+    document.getElementById("header").style.
+    backgroundColor = "blue"
+     document.getElementById("footer").style.
+    backgroundColor = "blue"
+});
+document.getElementById("voltar").
+addEventListener("click", function(){
+    document.getElementById("header").style.
+    backgroundColor = "black"
+     document.getElementById("footer").style.
+    backgroundColor = "black"
+});
